589cb73da1a8c4e29c41c9d3d8704ab24a9c0223
AnthonyChacin <anthony.chacin@correo.unimet.edu.ve>
2018-09-26 19:59:14 -0400

    Agregando la componente app-login, metiendo el codigo del login en dicha componente y metiendo el codigo del encabezado en la componente header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
