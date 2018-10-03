7fafce48747c0076bf7f2470261a67a53e6f06c2
AnthonyChacin <anthony.chacin@correo.unimet.edu.ve>
2018-10-03 16:23:50 -0400

    Agregando carpeta component dentro de app y migrando todas las componentes a esa carpeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './component/header/header.component';
 import { RegistroLoginComponent } from './view/registro-login/registro-login.component';
 import { AlertModule } from 'ngx-bootstrap';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FooterComponent } from './footer/footer.component';
-import { ComprasComponent } from './compras/compras.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ComprasComponent } from './component/compras/compras.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './view/home/home.component';
 import { LoginNavigationComponent } from './navigation/login-navigation/login-navigation.component';
