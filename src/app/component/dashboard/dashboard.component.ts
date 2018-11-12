c8aaf208c07209099db6db4fd3aafda73bb34155
AnthonyChacin <anthony.chacin@correo.unimet.edu.ve>
2018-11-11 21:47:46 -0400

    Construir orden, dashboard funcionable

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductsService } from '../../service/products/products.service';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() {
+	public products = [];
+
+  constructor(private productsService: ProductsService) {
    }
 
   ngOnInit() {
+  	this.productsService.getProducts().subscribe((productSnapshot) => {
+        this.products = [];
+        
+        productSnapshot.forEach((productData: any) => {
+        firebase.firestore().collection('/products/').doc(productData.payload.doc.id).onSnapshot((data) => {
+          if (data.get('plato') === 'Principal') {
+          	if(data.get('available') === 'Disponible'){
+          		this.products.push({
+	              id: productData.payload.doc.id,
+	              data: productData.payload.doc.data()
+	            })
+          	}
+          }
+        })
+      })
+    })
   }
 
 }
