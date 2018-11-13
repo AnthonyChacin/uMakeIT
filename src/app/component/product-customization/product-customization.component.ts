6a534080b10780b72699409672f04496d622c9ca
AnthonyChacin <anthony.chacin@correo.unimet.edu.ve>
2018-11-12 21:15:21 -0400

    Merge con ConstruirOrden

--- src/app/component/product-customization/product-customization.component.ts
+++ src/app/component/product-customization/product-customization.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ProductsService } from '../../service/products/products.service';
 import * as firebase from 'firebase';
 import { Product } from '../../models/product';
+import { Plate } from '../../models/plate';
+import { Order } from '../../models/order';
 
 @Component({
   selector: 'app-product-customization',
@@ -11,17 +13,104 @@ import { Product } from '../../models/product';
 export class ProductCustomizationComponent implements OnInit {
   @Input() name: String;
   public product = {} as Product;
-  public racion = {} as Product;
-  public aderezo = {} as Product;
-  public jugo = {} as Product;
-  public postre = {} as Product;
+  public plate = {} as Plate;
+  public order = {} as Order;
+  public racion = {} as Plate;
+  public aderezo = {} as Plate;
+  public jugo = {} as Plate;
+  public postre = {} as Plate;
   public raciones = [];
   public aderezos = [];
   public jugos = [];
   public postres = [];
   public productID: any;
+  public plato = [];
+  public orden = [];
 
   constructor(private productsService: ProductsService) { }
+
+  agregarPlato() {
+
+    if (this.name != "" && this.plate.cant != null) {
+      this.productsService.getProducts().subscribe((productSnapshot) => {
+        productSnapshot.forEach((productData: any) => {
+          if (productData.payload.doc.data().name === this.name) {
+            const reference = productData.payload.doc.id;
+            const plato: any = {
+              reference_plate: reference,
+              cant_plate: this.plate.cant,
+              products_plate: this.plato
+            }
+            firebase.firestore().collection('/plates/').add(plato);
+          }
+        })
+      })
+    }
+  }
+
+  agregarRacion() {
+    if (this.racion.name != "" && this.racion.cant != null) {
+      this.productsService.getProducts().subscribe((productSnapshot) => {
+        productSnapshot.forEach((productData: any) => {
+          if (productData.payload.doc.data().name === this.racion.name) {
+            this.plato.push({
+              id: productData.payload.doc.id,
+              cant: this.racion.cant
+            })
+            console.log(this.plato);
+          }
+        })
+      })
+    }
+  }
+
+  agregarAderezo() {
+    if (this.aderezo.name != "" && this.aderezo.cant != null) {
+      this.productsService.getProducts().subscribe((productSnapshot) => {
+        productSnapshot.forEach((productData: any) => {
+          if (productData.payload.doc.data().name === this.aderezo.name) {
+            this.plato.push({
+              id: productData.payload.doc.id,
+              cant: this.aderezo.cant
+            })
+            console.log(this.plato);
+          }
+        })
+      })
+    }
+  }
+
+  agregarJugo() {
+    if (this.jugo.name != "" && this.jugo.cant != null) {
+      this.productsService.getProducts().subscribe((productSnapshot) => {
+        productSnapshot.forEach((productData: any) => {
+          if (productData.payload.doc.data().name === this.jugo.name) {
+            this.plato.push({
+              id: productData.payload.doc.id,
+              cant: this.jugo.cant
+            })
+            console.log(this.plato);
+          }
+        })
+      })
+    }
+  }
+
+  agregarPostre() {
+    if (this.postre.name != "" && this.postre.cant != null) {
+      this.productsService.getProducts().subscribe((productSnapshot) => {
+        productSnapshot.forEach((productData: any) => {
+          if (productData.payload.doc.data().name === this.postre.name) {
+            this.plato.push({
+              id: productData.payload.doc.id,
+              cant: this.postre.cant
+            })
+            console.log(this.plato);
+          }
+        })
+      })
+    }
+  }
 
   ngOnInit() {
     this.productsService.getProducts().subscribe((productSnapshot) => {
